bc0d00ea9acd1bc68b68f45a6286faed93573c01
travis1999 <trevoroguna@gmail.com>
2021-04-28 10:07:31 +0300

    fixed mandatory format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,6 @@
 def uppercase(str):
     for x in str:
         convert = 96 < ord(x) < 173
-        print(chr(ord(x) - (32)) if convert else x, end="")
+        string = "{}".formant(chr(ord(x) - (32)) if convert else x)
+        print(string, end="")
     print()
