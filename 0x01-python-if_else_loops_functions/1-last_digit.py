286b82245f5ee2af71d9acf6bad492f9d10d1d9b
travis1999 <trevoroguna@gmail.com>
2021-04-28 09:49:25 +0300

    fixed extra and is

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,9 @@ str_format = "Last digit of {} is {} and is {}"
 last_digit = number % 10 if number > 0 else 10 - (number % 10)
 last_mess = ""
 if last_digit > 5:
-    last_mess = "and is greater than 5"
+    last_mess = "greater than 5"
 elif last_digit == 0:
-    last_mess = "and is zero"
+    last_mess = "zero"
 elif last_digit < 6 and last_digit != 0:
-    last_mess = "and is less than 6 and not 0"
+    last_mess = "less than 6 and not 0"
 print(str_format.format(number, last_digit, last_mess))
