4905a583310a4cf9fa7a26984b44b1310600552b
travis1999 <trevoroguna@gmail.com>
2021-04-29 21:49:29 +0300

    import protection?

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-print("{} arguments:".format(len(sys.argv) - 1))
-for idx, arg in enumerate(sys.argv[1:]):
-    print("{}: {}".format(idx + 1, arg))
+
+if __name__ == "__main__"
+    print("{} arguments:".format(len(sys.argv) - 1))
+    for idx, arg in enumerate(sys.argv[1:]):
+        print("{}: {}".format(idx + 1, arg))
