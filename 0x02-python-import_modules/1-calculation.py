5aa9493b625e5e65e2689b0242196679851889a8
travis1999 <trevoroguna@gmail.com>
2021-04-29 21:45:42 +0300

    fixed issue caused by importation of the wrong module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+from add_0 import add, sub, mul, div
 
 a = 10
 b = 5
 
-form = "{} {} {} = {}"
 
-print(form.format(a, '+', b, add(a, b)))
-print(form.format(a, '-', b, sub(a, b)))
-print(form.format(a, '*', b, mul(a, b)))
-print(form.format(a, '/', b, div(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
