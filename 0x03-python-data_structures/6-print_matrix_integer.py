d499726275b21a768add3a9a7286bc7b4f411b7e
travis1999 <trevoroguna@gmail.com>
2021-05-05 10:30:11 +0300

    integer formating fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return
     for x in matrix:
-        form = "{} "*len(x)
+        form = "{:d} "*len(x)
         print(form.format(*x).strip())
