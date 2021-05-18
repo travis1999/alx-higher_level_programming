9017c20c0f34f8957d7a3e6d4d4fe7e244131e90
travis1999 <trevoroguna@gmail.com>
2021-05-18 18:19:18 +0000

    added documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""Creating classes
+
+class square created with private var size
+
+"""
 
 
 class Square:
     def __init__(self, size):
+        """Square initializer
+
+        Args:
+            size (int): size of the square.
+
+        Returns:
+            None
+    """
         self.__size = size
