3ec07b0280d0124c8cb782f8e80368731c105cbf
travis1999 <trevoroguna@gmail.com>
2021-05-18 19:10:11 +0000

    pep8 fixes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
 
+""" Square class definition """
+
 
 class Square:
     def __init__(self, size=0, position=(0, 0)):
+        """ Square initializer
+
+            Args:
+                size (int): size of square
+                position (tuple): 2d position of the square
+            Returns:
+                None
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """ size getter """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """size setter
+
+            Args:
+                value (int): value of new size
+            Returns:
+                None
+        """
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
@@ -20,22 +38,39 @@ class Square:
 
     @property
     def position(self):
+        """position getter"""
         return self.__position
 
     @staticmethod
     def valid(x):
+        """checks args"""
         return type(x) == int and x >= 0
 
     @position.setter
     def position(self, value):
+        """size setter
+
+            Args:
+                value (int): value of new size
+            Returns:
+                None
+        """
         if False in [*[self.valid(x) for x in value], len(value) == 2]:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
+        """calculates area
+
+        Returns:
+            int: area of a square
+        """
         return self.__size * self.__size
 
     def my_print(self):
+        """
+            prints a square
+        """
         for x in range(self.size):
             print(" "*self.position[0]+"#"*self.size)
 
