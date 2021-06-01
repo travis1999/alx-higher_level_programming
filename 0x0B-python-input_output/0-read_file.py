212dc9571e027801b29fb01c5a4628abde44cee0
travis1999 <trevoroguna@gmail.com>
2021-06-01 22:07:06 +0300

    fixed encoding and pep8 style

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,9 @@
 
 def read_file(filename=""):
     """reads file to stdout"""
-    with open(filename, "r") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         print(file.read())
-    
+
+
+if __name__ == "__main__":
+    read_file(filename="student.json")
