a1db145971758a9c957bd21acd7d93deb5a0bb6a
travis1999 <trevoroguna@gmail.com>
2021-06-01 22:18:43 +0300

    added shebang

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,13 @@
-import json
+#!/bin/python3
 """to json module"""
+
+import json
 
 
 def to_json_string(my_obj):
     """returns json repr of a python obj"""
     return json.dumps(my_obj)
+
+
+if __name__ == "__main__":
+    print(to_json_string([1, 2, 3, 4]))
