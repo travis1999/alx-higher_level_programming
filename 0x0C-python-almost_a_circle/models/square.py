9d1ba500d7bf53b0119e6efdf10858d04424bd03
travis1999 <trevoroguna@gmail.com>
2021-06-08 23:25:15 +0300

    added missing  function documentation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,6 +19,7 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """Square as string"""
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                self.x, self.y, self.width)
 
