ffa1d5641940f9a4f58dfbbc23cf2cbc13ce1168
travis1999 <trevoroguna@gmail.com>
2021-07-22 21:30:39 +0300

    fixes

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""is sub"""
+"""is_kind_of_class function"""
 
 
 def is_kind_of_class(obj, a_class):
-    """is subclass"""
-    return isinstance(obj, a_class):
+    """returns True or False based on inheritance"""
+    return isinstance(obj, a_class)
