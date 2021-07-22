813cd8903e2ef98d773e5e7d5732b8398c1bb277
travis1999 <trevoroguna@gmail.com>
2021-07-22 15:34:44 +0300

    pep 8 fixes

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -3,62 +3,62 @@
 
 
 class Rectangle:
-	"""Rectangle class def"""
-	
-	number_of_instances = 0
- 
-	def __init__(self, width=0, height=0):
-		"""class initializer"""
-		self.width = width
-		self.height = height
-		Rectangle.number_of_instances += 1
+    """Rectangle class def"""
 
-	def __str__(self):
-		if self.width == 0 or self.height == 0:
-			return ""
-		return ("#"*self.width + "\n") * self.height
+    number_of_instances = 0
 
-	def __repr__(self):
-		return "Rectangle({}, {})".format(self.width, self.height)
+    def __init__(self, width=0, height=0):
+        """class initializer"""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
 
-	def __del__(self):
-		print("Bye rectangle...")
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        return ("#"*self.width + "\n") * self.height
 
-	def check_dim(self, value, name):
-		"""checks if dimention is valid"""
-		if type(value) != int:
-			raise TypeError("{} must be an integer".format(name))
-		if value < 0:
-			raise ValueError("{} must be >= 0".format(name))
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.width, self.height)
 
-	@property
-	def width(self):
-		"""width getter"""
-		return self.__width
+    def __del__(self):
+        print("Bye rectangle...")
 
-	@width.setter
-	def width(self, value):
-		"""width setter"""
-		self.check_dim(value, "width")
-		self.__width = value
+    def check_dim(self, value, name):
+        """checks if dimention is valid"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0:
+            raise ValueError("{} must be >= 0".format(name))
 
-	@property
-	def height(self):
-		"""height getter"""
-		return self.__height
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
 
-	@height.setter
-	def height(self, value):
-		"""width setter"""
-		self.check_dim(value, "height")
-		self.__height = value
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        self.check_dim(value, "width")
+        self.__width = value
 
-	def area(self):
-		"""returns the area of the Rectangle"""
-		return self.width * self.height
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
 
-	def perimeter(self):
-		"""returns the perimeter of the rectangle"""
-		if self.width == 0 or self.height == 0:
-			return 0
-		return 2*(self.width+self.height)
+    @height.setter
+    def height(self, value):
+        """width setter"""
+        self.check_dim(value, "height")
+        self.__height = value
+
+    def area(self):
+        """returns the area of the Rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """returns the perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2*(self.width+self.height)
