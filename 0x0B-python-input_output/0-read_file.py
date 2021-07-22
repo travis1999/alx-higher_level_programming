7050d8392591e69b4103a046b2efd91b4c332b14
travis1999 <trevoroguna@gmail.com>
2021-07-22 15:41:05 +0300

    print format fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,8 +5,4 @@
 def read_file(filename=""):
     """reads file to stdout"""
     with open(filename, "r", encoding="utf-8") as file:
-        print(file.read())
-
-
-if __name__ == "__main__":
-    read_file("student.json")
+        print(file.read(), end="")
