bc9a77b3aba11cfebe200c39a9de2ad748d16294
travis1999 <trevoroguna@gmail.com>
2021-08-03 00:32:18 +0300

    8-square.js: used correct character to print
    character 'X' instead of '#'

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,7 +6,7 @@ if (isNaN(process.argv[2])) {
 } else {
   for (let idy = 0; idy < parseInt(process.argv[2]); idy++) {
     for (let index = 0; index < parseInt(process.argv[2]); index++) {
-      process.stdout.write('#');
+      process.stdout.write('X');
     }
     console.log();
   }
