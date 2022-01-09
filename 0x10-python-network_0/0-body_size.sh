266ccb503be2a76c772e876dae652725293af577
travis1999 <trevoroguna@gmail.com>
2022-01-10 00:16:44 +0300

    initial commit

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Bash script that takes in a URL, sends a 
+#request to that URL, and displays the size 
+#of the body of the response
+
+curl -s $1 --include | grep -i Content-Length | awk '{print $2}
